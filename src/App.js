fc73a4890b7e83cc1ad30dc762a2c598de682160
Deron Franklin Sobers II <deronfsobers2@gmail.com>
2021-08-07 19:06:05 -0500

    based-project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="App-outer-logo" alt="logo"></div>
+      <div className="App-inner-logo" alt="inner-logo"></div>
       </header>
+      <p>
+          <code>Unconditional Love fertilizes relationships</code>
+        </p>
+        <p className="credit">* Digital art created by Deron Franklin Sobers II *</p>
     </div>
   );
 }
